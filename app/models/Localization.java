d9ef98c954fa37e202f16d97eebaa9434a15ade2
Tobias Wallura <tobias@wallura.org>
2013-05-23 00:48:53 +0200

    fill jQuery table with values from controller

--- app/models/Localization.java
+++ app/models/Localization.java
@@ -1,6 +1,13 @@
 package models;
 
 public class Localization {
+
+    public Localization(String key, String value, String locale) {
+        this.key = key;
+        this.value = value;
+        this.locale = locale;
+    }
+
     public String key;
     public String value;
     public String locale;
