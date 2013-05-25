55b58a632a0b1e399dc5aaa29a82ac3e72871432
Tobias Wallura <tobias@wallura.org>
2013-05-26 01:56:29 +0200

    ported test-application to Play 2.1

--- test-application/app/controllers/LocalizedApplication.java
+++ test-application/app/controllers/LocalizedApplication.java
@@ -1,31 +1,53 @@
 package controllers;
 
-import org.apache.commons.lang.StringUtils;
+import static play.data.Form.form;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import play.data.DynamicForm;
 import play.i18n.Lang;
 import play.i18n.Messages;
 import play.mvc.Controller;
+import play.mvc.Result;
 
 /**
  * @author huljas
  */
 public class LocalizedApplication extends Controller {
 
-    public static void hello(String name) {
+    public static List<String> getLangs() {
+        List<String> list = new ArrayList<>();
+        for (Lang i : Lang.availables()) {
+            list.add(i.code());
+        }
+
+        return list;
+    }
+
+    public static Result hello() {
+        DynamicForm f = form().bindFromRequest();
+        String name = f.get("name");
         if (StringUtils.isBlank(name)) {
             name = Messages.get("you");
         }
         String hello = Messages.get("hello.type.normal", name);
-        render(hello);
+        return ok(views.html.hello.render(hello));
     }
 
-    public static void helloWithType(String name, String type) {
+    public static Result helloWithType() {
+        DynamicForm f = form().bindFromRequest();
+        String name = f.get("name");
+        String type = f.get("type");
         String hello = Messages.get("hello.type." + type, name);
-        render("@hello", hello);
+        return ok(views.html.hello.render(hello));
     }
 
-    public static void setLang(String lang) {
-        Lang.change(lang);
-        hello(null);
+    public static Result setLang(String lang) {
+        changeLang(lang);
+        return hello();
     }
 
 }
